242ecd3338e28e39045acb8c340621cb8f3d5b9f
sinmirka <oscarka65@gmail.com>
2026-09-07 19:06:47 +0500

    feat: add text extraction from segments

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from faster_whisper import WhisperModel
+from pathlib import Path
+
+test_video = Path(__file__).parent / 'whisper_test.mp4'
 
 model_size = "small"
 
-model = WhisperModel(model_size, device='cuda', compute_type='float16')
+model = WhisperModel(model_size, device='cpu', compute_type='int8')
 
-segments, info = model.transcribe("")
+segments, info = model.transcribe(test_video)
+print('started')
+for segment in segments:
+    print(f"[{segment.start} -> {segment.end}] {segment.text}")
