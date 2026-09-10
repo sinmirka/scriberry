5aa4c62e7e1c74598fb3f0d83777fd3e10c8ad8b
sinmirka <oscarka65@gmail.com>
2026-09-10 20:03:03 +0500

    perf: move test_video to test file

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from PySide6.QtWidgets import QApplication
 from scriberry.transcriber import WhisperTranscriber
 from scriberry.gui.main_window import MainWindow
 
-test_video = Path(__file__).parent / 'whisper_test.mp4'
-
 app = QApplication([])
 
 window = MainWindow()
