6ba98aa1d46c9bef6e4c3d49bd3c2d08e8d35a25
sinmirka <oscarka65@gmail.com>
2026-09-11 17:39:21 +0500

    feat: add source selection

--- scriberry/gui/main_window.py
+++ scriberry/gui/main_window.py
@@ -1,4 +1,5 @@
-from PySide6.QtWidgets import QMainWindow
+from pathlib import Path
+from PySide6.QtWidgets import QMainWindow, QFileDialog, QTreeWidgetItem
 
 from .ui_scriberry import Ui_ScriberryWindow
 
@@ -8,3 +9,38 @@ class MainWindow(QMainWindow):
 
         self.ui = Ui_ScriberryWindow()
         self.ui.setupUi(self)
+
+        self._selected_files: list[Path] = []
+        self._output_directory: Path | None = None
+
+        self._connect_signals()
+
+    def _connect_signals(self):
+        self.ui.selectVideoButton.clicked.connect(
+            self._select_source
+        )
+
+    def _select_source(self):
+        file_path, _ = QFileDialog.getOpenFileName(
+            self,
+            filter="Видео (*.mp4 *.mkv *.avi *.mov *.webm)"
+        )
+        if not file_path:
+            return
+
+        self._selected_files.append(file_path)
+        print("\n".join(self._selected_files))
+
+        path = Path(file_path)
+        size_mb = path.stat().st_size / (1024 * 1024)
+
+        item = QTreeWidgetItem([
+            path.name,
+            f"{size_mb:.1f} MB",
+            "Ожидает"
+        ])
+
+        self.ui.filesTreeWidget.addTopLevelItem(item)
+        self.ui.fileCountLabel.setText(
+            f"{len(self._selected_files)} файлов"
+        )
